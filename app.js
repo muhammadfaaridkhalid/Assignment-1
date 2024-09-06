3ad09f6c4a5e1e05b509c0508ee0e500d5f5324d
Muhammad Faarid Khalid <khalidhabibe19@gmail.com>
2024-09-06 16:04:54 +0500

    add filed and folders

---
+++ app.js
@@ -0,0 +1,224 @@
+// chapter 1 Name Is (alert)
+
+
+// Question no 1  
+
+// alert("Hello!"); 
+
+// Question no 2 
+
+// alert("Error: please enter a valid password");
+
+// Question no 3 
+
+// alert("Welcome to JS Land... Happy Coding!") 
+
+// Question no 4 
+
+// alert("Welcome to Js land..")
+// alert("Happy Coding!")
+
+// Question no 5 
+
+// console.log("Hello.. I can run JS through my web browser's console")
+
+// Question no 7
+
+// a.yes 
+// b.yes 
+// c.yes 
+// d.yes 
+
+// Chapter no 2 (variables for string)
+
+// Question no 1
+
+// var username;
+
+// Question no 2 
+
+// var myName = "Muhammad Faarid Khalid"
+
+// Question no 3
+
+// a. var simpletext;
+
+// b. var simpletext = "hello world"
+
+// alert("hello World")
+
+// Question no 4
+
+// alert("Jhone Doe")
+
+// alert("15 years old")
+
+// alert("Ceertified mobile application development")
+
+// Question no 5
+
+// alert("pizza\npizz\npiz\npi\np")
+
+// Question no 6
+
+// var email = "khalidhabibe19@gmail.com"
+
+// alert(email)
+
+// Question no 7
+
+// var bookName = "A smarter way to learn JavaScript"
+
+// alert("I am trying to learn the book " + bookName)
+
+// Question no 8 
+
+// document.write("Yeh! I Can Write HTML content through Javascript ")
+
+// Question no 9 
+
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”")
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Chapter No 3 (Variables for numbers )
+
+// Q1 
+
+// var age = "I am 17 years old"
+
+// alert(age)
+
+// Q2 
+
+// var website = "You have visited this site 100 times"
+
+// alert(website)
+
+// Q3 
+
+// var birthdate =  "my birth year is 2008"
+
+// document.write(birthdate)
+
+// document.write(typeof birth)
+
+// Q4 
+
+// var V_abc = "John Doe"
+
+// var P_title = "5 T-shirt(s)"
+// var cloth = "XYZ Clothing store."
+// document.write(V_abc + "ordered " + P_title + "on " + cloth)
+
+// Chater No 4 (variables names legal and illegal )
+
+// Q1 
+// var name = "Muhammad Faarid Khalid", city = "karachi", age = 17
+
+// Q2
+
+// legal 
+
+// var fullname
+
+// var $name
+
+// var name1
+
+// var last_name 
+
+// var FullName
+
+// illegal 
+
+// var 12name 
+
+// var #name
+
+// var name#
+
+// var !myname
+
+// var last-name 
+
+//  Chapter No 2 Name Is Word File 
+
+// Q1 
+
+// alert("Faarid Khalid")
+
+
+// alert("Khalid")
+
+
+// alert("khaildhabibe19@gmail.com")
+
+
+// alert("03012715433")
+
+
+// alert("Faarid#987")
+
+
+// Q2 
+
+// alert("You're learning JavaScript!")
+
+// Q3 
+// alert("Hello, my name is Muhammad Faarid and I am in first year")
+
+// Chapter 2 Name Is (Variables for string)
+
+// Q1 
+
+// Q2 
+// var first_name;
+// first_name = "Hello World"
+
+// Q3 
+// var TeamName = "my team members is faarid, zaid and ahmed"
+// alert(TeamName)
+
+// Q4 
+// var bestMan = "Charlie"
+
+// bestMan = "new Charlie"
+
+// Chapter 3 (Variables for numbers) 
+
+// Q1 
+
+// var caseQty
+
+// Q2 
+
+// caseQty = 144 
+
+// Q3 
+
+// var num = 9
+
+// Q4 
+
+// var my_num
+
+// my_num = 2+20
+
+// Q5 
+
+// var merchTotal = 100;
+
+// var ShippingCharge = 10;
+
+// var Totalorder = merchTotal + ShippingCharge;
+
+// console.log(Totalorder)
+
+// Q6 
+
+// var num = 50
+
+// totalnum = number + 50
+
+// console.log(totalnum)
